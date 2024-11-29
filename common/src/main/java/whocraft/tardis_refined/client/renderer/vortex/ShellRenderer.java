9ca42265c66b8caf25509569316ec14e3792b770
Edrax Doodles <edusgprnetwork@gmail.com>
2024-11-28 23:54:01 -0300

    changed vortex tardis anim back

--- common/src/main/java/whocraft/tardis_refined/client/renderer/vortex/ShellRenderer.java
+++ common/src/main/java/whocraft/tardis_refined/client/renderer/vortex/ShellRenderer.java
@@ -48,9 +48,10 @@ public class ShellRenderer {
             float yR = ((timeFactor * 360 / (float) (2 * Math.PI)) % 360) * throttle; // Continuous spin on Y-axis
             float zR = (float) Math.cos(timeFactor * 3) * 10.0f; // Wobble on Z-axis
 
-            pose.translate(0, -1.5, 0);
-            RenderHelper.rotateZYX(pose, xR * control, yR * control, zR * control);
-            pose.translate(0, 1.5, 0);
+            //pose.translate(0, -1.5, 0);//yR * control
+            RenderHelper.rotateZYX(pose, xR * control, 0, zR * control);
+            RenderHelper.rotateZYX(pose, 0, yR * control, 0);
+            //pose.translate(0, 1.5, 0);
             VertexConsumer vertexConsumer = guiGraphics.bufferSource().getBuffer(model.renderType(model.getShellTexture(ShellPatterns.getPatternOrDefault(shellTheme, shellPattern), false)));
             RenderSystem.enableBlend();
             globalShellBlockEntity.setTardisId(tardisClientData.getLevelKey());
